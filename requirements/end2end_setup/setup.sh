92d5064a44c0e46bf8f81915284a9c2f16b83a39
twan3617 <tonywang205@yahoo.com.au>
2022-03-06 14:10:06 +1100

    image

---
+++ requirements/end2end_setup/setup.sh
@@ -0,0 +1,4 @@
+conda create --name end2end_env python=3.7.12 pip=21.2.2
+conda activate end2end_env
+pip install -r end2end_requirements.txt
+
---
+++ requirements/tf_setup/setup.sh
@@ -0,0 +1,4 @@
+conda create --name tf_env_anomaly python=3.7.12 pip=21.2.2
+conda activate tf_env_anomaly 
+pip install -r tf_requirements.txt
+
